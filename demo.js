ca4a06400dc86054cd94329673a38a74f2b09b94
zhangxiaolei <fttat@163.com>
2025-06-28 20:10:00 +0800

    完善dev代码

--- demo.js
+++ demo.js
@@ -1,7 +1,2 @@
-window.onload = function() {    
-    let box = document.getElementsByClassName("box")[0];
-    // 或者：let box = document.querySelector(".box");
-    box.addEventListener("click", ()=>  {
-        alert("这是测试代码");
-    });
-}
+console.log("这是测试代码")
+console.log("这是dev分支")
